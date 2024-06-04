5d00990a4dcf9b8325b5c4735748e8e01dc1f341
z3nbunny <starfire3d@gmail.com>
2024-06-04 15:32:13 +0000

    simple variables creation

--- src/examples/b_input_proc_output/main.py
+++ src/examples/b_input_proc_output/main.py
@@ -1,3 +1,8 @@
-import input_process_output
+whole_number = 10 #integer variable
+print(whole_number + 10)
 
-input_process_output.display_output()
+float_number = 10.99 #float varaiable
+print(float_number + 5.55)
+
+name = "Python" #a literal string
+print(name + " is cool")
