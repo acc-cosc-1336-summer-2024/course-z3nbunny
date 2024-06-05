32fd47190ed205021d19c473865e910afbcbab53
z3nbunny <starfire3d@gmail.com>
2024-06-05 03:14:46 +0000

    integer floating division modulus operator

--- src/examples/b_input_proc_output/main.py
+++ src/examples/b_input_proc_output/main.py
@@ -1,17 +1,13 @@
-import input_process_output #references input_process_output.py
+import input_process_output #references input_process_output.py file
 
-value = input("Enter and number: ") #capture keyboard data
+value1 = 10
+value2 = 4
 
-integer = input_process_output.echo_number(value) #echo_number function is in the input_process_output.py file
-print(integer)
+result = value1 / value2 #will do decimal operation
+print(result)
 
-value = input("Enter a decimal number: ")
+result = value1 // value2 #will do a whole number operation
+print(result)
 
-float = input_process_output.echo_decimal_number(value) #echo_decimal function is in the input_process_output.py file
-print(float)
-
-value = input("Enter a sequence of chars: ")
-
-string = input_process_output.echo_string(value)#echo_string function is in the input_process_output file
-
-print(string) #print is built in
+result = 10 % 4 
+print(result)
