c6b9f76a9664f6341a780f2aa45b605c0eb14acb
z3nbunny <starfire3d@gmail.com>
2024-06-06 02:44:08 +0000

    Homework 2

--- run_tests.py
+++ run_tests.py
@@ -6,7 +6,7 @@ has the test functions
 #from tests.homework.b_in_proc_out import tests_in_proc_out
 
 
-from tests.examples.c_decisions import tests_decisions
+from tests.homework.b_in_proc_out import tests_in_proc_out
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_decisions)
+suite = unittest.TestLoader().loadTestsFromModule(tests_in_proc_out)
 unittest.TextTestRunner(verbosity=2).run(suite)
