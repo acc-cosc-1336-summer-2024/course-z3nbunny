e947e5874f02ee66ca3f8d43505285e468a9b832
z3nbunny <starfire3d@gmail.com>
2024-06-06 00:21:29 +0000

    intro to else

--- src/examples/c_decisions/main.py
+++ src/examples/c_decisions/main.py
@@ -6,4 +6,7 @@ result = decisions.is_number_not_in_range(int(value1), 1, 10) #convert value to 
 
 if(result == True):
     print(value1, " not in range")
+else:
+    print(value1, " in range")
+    
 print(result)
