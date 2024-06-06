e9991c09bc98197b5242409bb2b36492acedabe2
z3nbunny <starfire3d@gmail.com>
2024-06-06 00:18:03 +0000

    intro to if statements

--- src/examples/c_decisions/main.py
+++ src/examples/c_decisions/main.py
@@ -3,4 +3,7 @@ import decisions
 value1 = input("Enter a number: ")#read from keyboard, Python brings in values as a string variable
 
 result = decisions.is_number_not_in_range(int(value1), 1, 10) #convert value to integer number with int function
+
+if(result == True):
+    print(value1, " not in range")
 print(result)
