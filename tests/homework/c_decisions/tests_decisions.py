54ccb2b97b35394156f3b9f9eab785605769faf5
z3nbunny <starfire3d@gmail.com>
2024-06-14 21:26:03 +0000

    2nd test of hw3

--- tests/homework/c_decisions/tests_decisions.py
+++ tests/homework/c_decisions/tests_decisions.py
@@ -1,1 +1,26 @@
-#
+import unittest
+from src.homework.c_decisions.decisions import get_faulty_rating, get_options_ratio
+from src.homework.c_decisions.decisions import get_faculty_rating
+
+class TestConfig(unittest.TestCase):
+    def test_get_options_ratio_1(self):
+        self.assertEqual(0.25, get_options_ratio(5, 20))
+
+class TestConfig(unittest.TestCase):
+    def test_get_options_ratio_1(self):
+        self.assertEqual(0.5, get_options_ratio(10, 20))
+
+    def test_get_faulty_rating_excellent(self):
+        self.assertEqual("Excellent", get_faulty_rating())
+
+    def test_get_faulty_rating_very_good(self):
+        self.assertEqual("Very Good", get_faulty_rating())
+
+    def test_get_faulty_rating_very_good(self):
+        self.assertEqual("Good", get_faulty_rating())
+
+    def test_get_faulty_rating_very_good(self):
+        self.assertEqual("Needs Improvement", get_faulty_rating())
+
+    def test_get_faulty_rating_very_good(self):
+        self.assertEqual("Unacceptable", get_faulty_rating())
