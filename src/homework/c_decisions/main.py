9469d237627b64684a8875bd33930f86cdd4740d
z3nbunny <starfire3d@gmail.com>
2024-06-15 02:52:09 +0000

    Homework 3

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,6 +1,12 @@
 #
 import decisions
 
+options = float( input ("Please enter options: ") )
 
+total = float( input ("Please enter total: ") )
 
-professor_rating = input ("Enter a rating for your professor: ")
+ratio = decisions.get_options_ratio(options, total)
+
+rating = decisions.get_faculty_rating(ratio)
+
+print(rating)
