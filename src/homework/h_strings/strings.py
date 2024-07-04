58f4ec2c98972c3d385b1c9e5a4e6dab54a69ae7
z3nbunny <starfire3d@gmail.com>
2024-07-04 02:45:59 +0000

    get_hamming_distance and get_dna_complement functions finished

--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,1 +1,28 @@
 #
+def get_hamming_distance(dna1, dna2):
+    hamming_distance = 0
+    for i in range(len(dna1)):
+        if dna1[i] != dna2[i]:
+            hamming_distance += 1
+    return hamming_distance
+    
+def get_dna_complement(dna):
+
+    complements_dict = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+
+    dna_reversed  = []
+
+    for dna_base in range(len(dna) -1, -1, -1):
+        dna_reversed.append(dna[dna_base])
+
+    dna_complement = []
+
+    for dna_base in dna_reversed:
+        dna_complement.append(complements_dict[dna_base])
+    
+    result = ''
+
+    for bases in dna_complement:
+        result += bases
+    
+    return result
