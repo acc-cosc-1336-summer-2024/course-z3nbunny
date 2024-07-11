4bab1f2a51d6f1bed028521094911c6c58333096
z3nbunny <starfire3d@gmail.com>
2024-07-11 03:22:35 +0000

    Functions for hw 6

--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,30 @@
 #
+def get_p_distance(list1, list2):
+
+    differences = 0
+    list_length = len(list1)
+
+    for i in range(list_length):
+        if list1[i] != list2[i]:
+            differences +=1
+    p_distance = differences / list_length
+    return p_distance
+
+def get_p_distance_matrix(list1):
+
+    list_length = len(list1)
+
+    p_distance_matrix = []
+
+    for i in range(list_length):
+        row = [0] * list_length
+        p_distance_matrix.append(row)
+    
+    for i in range(list_length):
+        for j in range(list_length):
+            if i != j:
+                p_distance_matrix[i][j] = get_p_distance(list1[i], list1[j])
+            else:
+                p_distance_matrix[i][j] = 0.0
+    
+    return p_distance_matrix
