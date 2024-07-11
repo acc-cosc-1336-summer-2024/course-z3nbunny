fc755bc59bb045acafc5aea18ab7aeea9094a9ce
z3nbunny <starfire3d@gmail.com>
2024-07-11 04:33:29 +0000

    Working on main.py for hw6

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,1 +1,31 @@
-#
+import dictionary
+
+def get_p_distance_matrix_menu():
+    exit_choice = False
+    while not exit_choice:
+
+        print("Welcome to Homework 6 menu, options:")
+        print("1 - Get P Distance Matrix")
+        print("2 - Exit")
+
+        menu_selection = int(input("Enter Selection: "))
+    
+        if menu_selection == 1:
+            dna1 = input('Please enter DNA List: ')
+            p_distance = dictionary.get_p_distance_matrix(dna1)
+            print ('P Distance: ' + p_distance)
+
+        elif menu_selection == 2:
+            print("Exiting Homework 6, have a great day!")
+            exit_choice = True
+
+        else:
+            print("Sorry, that selection is invalid. Please select a number from 1 to 2.")
+
+        if not exit_choice:
+            exit_question = int(input("Enter 2 to exit now, or another number to return to the main menu: "))
+            if exit_question == 2:
+                exit_choice = True
+    print("Thank you, come again!")
+
+get_p_distance_matrix_menu()
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,22 @@
 #
+import unittest
+from src.homework.i_dictionaries_sets import get_p_distance
+from src.homework.i_dictionaries_sets import get_p_distance_matrix
+
+class Test_Config(unittest.TestCase):
+    def test_p_distance(self):
+        self.assertEqual(4, get_p_distance(['T','T','T','C','C','A','T','T','T','A'], ['G','A','T','T','C','A','T','T','T','C']))
+
+    def test_get_p_distance_matrix(self):
+        self.assertEqual([
+ [0.0, 0.4, 0.1, 0.1],
+ [0.4, 0.0, 0.4, 0.3],
+ [0.1, 0.4, 0.0, 0.2],
+[0.1, 0.3, 0.2, 0.0]
+], get_p_distance_matrix(
+[
+ ['T','T','T','C','C','A','T','T','T','A'],
+ ['G','A','T','T','C','A','T','T','T','C'],
+ ['T','T','T','C','C','A','T','T','T','T'],
+ ['G','T','T','C','C','A','T','T','T','A']
+]))
