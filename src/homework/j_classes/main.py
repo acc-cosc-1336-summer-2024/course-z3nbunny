108b9149b2a7c31198520f0f14e7442161ceef35
z3nbunny <starfire3d@gmail.com>
2024-07-18 04:54:55 +0000

    Hw 7 complete

--- src/homework/j_classes/main.py
+++ src/homework/j_classes/main.py
@@ -1,1 +1,12 @@
-#
+from class_a import Die
+
+def roll_the_die():
+    die = Die()
+    while True:
+        die.roll()
+        print(f"You rolled a {die.get_rolled_value()}")
+        roll_again = input("Roll again? (y/n): ")
+        if roll_again != "y":
+            break
+
+roll_the_die()
