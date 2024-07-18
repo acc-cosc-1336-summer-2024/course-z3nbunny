7aa94dd2e5d58aee60fe14ee75085433ce7b1436
z3nbunny <starfire3d@gmail.com>
2024-07-18 04:34:26 +0000

    hw7 tests complete

--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -1,1 +1,22 @@
-#
+import unittest
+from src.homework.j_classes.class_a import Die
+
+class Test_Config(unittest.TestCase):
+
+    def test_get_rolled_value_1 (self):
+        die = Die()
+        die.roll()
+        roll_value = die.get_rolled_value()
+        self.assertTrue(1 <= roll_value <= 6, f"The die roll value {roll_value} is outside of the expected range")
+
+    def test_get_rolled_value_2 (self):
+        die = Die()
+        die.roll()
+        roll_value = die.get_rolled_value()
+        self.assertTrue(1 <= roll_value <= 6, f"The die roll value {roll_value} is outside of the expected range")
+
+    def test_get_rolled_value_3 (self):
+        die = Die()
+        die.roll()
+        roll_value = die.get_rolled_value()
+        self.assertTrue(1 <= roll_value <= 6, f"The die roll value {roll_value} is outside of the expected range")
