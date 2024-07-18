85f6ed05385598007105d329823a1fb7478892cd
z3nbunny <starfire3d@gmail.com>
2024-07-18 03:18:03 +0000

    class Die complete

--- src/homework/j_classes/class_a.py
+++ src/homework/j_classes/class_a.py
@@ -1,1 +1,12 @@
-#
+from random import randint
+
+class Die:
+
+    def roll(self):
+        self._roll_value = randint(1, 6)
+
+    def get_rolled_value(self):
+        return self._roll_value
+    
+    def __str__(self):
+        return f"The rolled value is {self._roll_value}"
